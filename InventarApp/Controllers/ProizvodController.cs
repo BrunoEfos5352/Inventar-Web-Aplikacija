58c0c363bcf88150a22d39f574955d79eb58bffc
migiv <migiv@IVA-PC>
2026-01-17 21:08:48 +0100

    Add project files.

---
+++ InventarApp/Controllers/ProizvodController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using InventarApp.Models;
+using System.Linq;
+
+namespace InventarApp.Controllers
+{
+    public class ProizvodController : Controller
+    {
+        private static List<Proizvod> _proizvodi = new List<Proizvod>
+        {
+            new Proizvod { Id = 1, Naziv = "Laptop", Cijena = 800, Kolicina = 5 }
+        };
+
+        public IActionResult Index() => View(_proizvodi);
+
+        public IActionResult Create() => View();
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Proizvod novi)
+        {
+            if (ModelState.IsValid)
+            {
+                novi.Id = _proizvodi.Any() ? _proizvodi.Max(x => x.Id) + 1 : 1;
+                _proizvodi.Add(novi);
+                return RedirectToAction("Index");
+            }
+            return View(novi);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var p = _proizvodi.FirstOrDefault(x => x.Id == id);
+            if (p == null)
+            {
+                return NotFound();
+            }
+            return View(p);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Proizvod editirani)
+        {
+            if (id != editirani.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                var stari = _proizvodi.FirstOrDefault(x => x.Id == id);
+                if (stari != null)
+                {
+                    stari.Naziv = editirani.Naziv;
+                    stari.Cijena = editirani.Cijena;
+                    stari.Kolicina = editirani.Kolicina;
+                }
+                return RedirectToAction("Index");
+            }
+            return View(editirani);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var p = _proizvodi.FirstOrDefault(x => x.Id == id);
+            if (p == null)
+            {
+                return NotFound();
+            }
+            return View(p);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var p = _proizvodi.FirstOrDefault(x => x.Id == id);
+            if (p != null)
+            {
+                _proizvodi.Remove(p);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ InventarApp/Models/Proizvod.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InventarApp.Models
+{
+    public class Proizvod
+    {
+        public int Id { get; set; }
+        
+        [Required(ErrorMessage = "Naziv je obavezan")]
+        [StringLength(100, ErrorMessage = "Naziv može imati maksimalno 100 znakova")]
+        public string Naziv { get; set; }
+        
+        [Required(ErrorMessage = "Cijena je obavezna")]
+        [Range(0.01, 1000000, ErrorMessage = "Cijena mora biti između 0.01 i 1,000,000")]
+        [Display(Name = "Cijena (€)")]
+        public double Cijena { get; set; }
+        
+        [Required(ErrorMessage = "Količina je obavezna")]
+        [Range(0, 100000, ErrorMessage = "Količina mora biti između 0 i 100,000")]
+        public int Kolicina { get; set; }
+
+        public double IzracunajUkupnuVrijednost()
+        {
+            return Cijena * Kolicina;
+        }
+    }
+}
